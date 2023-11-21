aff0397b6be0cdbcf9319a800d87f1287c043521
Alexander Gutierrez <alexzgut.dev@gmail.com>
2023-11-21 13:57:48 -0500

    First commit

---
+++ TermProject_CSD3464/src/application/MainController.java
@@ -0,0 +1,51 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+public class MainController {
+	@FXML
+	private BorderPane bpContainer;
+
+	@FXML
+	private Button btnInsert;
+
+	@FXML
+	private Button btnShowRecords;
+	
+	public void initialize() {
+		try {
+			AnchorPane loader = FXMLLoader.load(getClass().getResource("PatientRecords.fxml"));
+			bpContainer.setCenter(loader);
+			btnShowRecords.setVisible(false);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void btnInsertListener() {
+		try {
+			AnchorPane loader = FXMLLoader.load(getClass().getResource("AddRecord.fxml"));
+			bpContainer.setCenter(loader);
+			btnShowRecords.setVisible(true);
+			btnInsert.setVisible(false);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void btnbtnShowRecordsListener() {
+		try {
+			AnchorPane loader = FXMLLoader.load(getClass().getResource("PatientRecords.fxml"));
+			bpContainer.setCenter(loader);
+			btnShowRecords.setVisible(false);
+			btnInsert.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
